e504cb5ad95730dbed06d8656407dc6238631573
Jaron Armiger <jaron.armiger@gmail.com>
2023-03-29 15:06:51 -0700

    implement textRoll

---
+++ src/features/textRoll.js
@@ -0,0 +1,105 @@
+import gsap from 'gsap'
+import $ from 'jquery'
+import SplitType from 'split-type'
+
+console.log(gsap)
+console.log($)
+
+// .text-roll
+// .text-roll-link
+
+const textRollTriple = (innerClassname, outerClassname) => {
+  let splitType = new SplitType(innerClassname, {
+    types: 'words, chars',
+    tagName: 'span',
+  })
+  console.log(splitType)
+
+  $(outerClassname).each(function () {
+    let firstChars = $(this).find(`${innerClassname}.is-1 .char`)
+    let secondChars = $(this).find(`${innerClassname}.is-2 .char`)
+    let thirdChars = $(this).find(`${innerClassname}.is-3 .char`)
+
+    let tl = gsap.timeline({ repeat: -1 })
+    tl.to(firstChars, {
+      translateY: '-0.2em',
+      rotationY: '-5.7deg',
+      rotationX: '-90deg',
+      stagger: { each: 0.08 },
+      ease: 'power3.inOut',
+      duration: 0.7,
+    })
+      .from(
+        secondChars,
+        {
+          translateY: '0.2em',
+          rotationY: '5.7deg',
+          rotationX: '90deg',
+          stagger: { each: 0.08 },
+          ease: 'power3.inOut',
+          duration: 0.7,
+        },
+        0.1
+      )
+      .to(secondChars, {
+        translateY: '-0.2em',
+        rotationY: '-5.7deg',
+        rotationX: '-90deg',
+        stagger: { each: 0.08 },
+        ease: 'power3.inOut',
+        duration: 0.7,
+      })
+      .from(
+        thirdChars,
+        {
+          translateY: '0.2em',
+          rotationY: '5.7deg',
+          rotationX: '90deg',
+          stagger: { each: 0.08 },
+          ease: 'power3.inOut',
+          duration: 0.7,
+        },
+        2
+      )
+      .to(thirdChars, {
+        translateY: '-0.2em',
+        rotationY: '-5.7deg',
+        rotationX: '-90deg',
+        stagger: { each: 0.08 },
+        ease: 'power3.inOut',
+        duration: 0.7,
+      })
+      .from(
+        firstChars,
+        {
+          translateY: '0.2em',
+          rotationY: '5.7deg',
+          rotationX: '90deg',
+          stagger: { each: 0.08 },
+          ease: 'power3.inOut',
+          duration: 0.7,
+        },
+        3.75
+      )
+    // .from(
+    //   thirdChars,
+    //   {
+    //     translateY: '0.2em',
+    //     rotationY: '5.7deg',
+    //     rotationX: '90deg',
+    //     stagger: { each: 0.08 },
+    //     ease: 'power3.inOut',
+    //     duration: 0.7,
+    //   },
+    //   0.1
+    // )
+    // $(this).on('mouseenter', function () {
+    //   tl.restart()
+    // })
+    // $(this).on('mouseleave', function () {
+    //   tl.reverse()
+    // })
+  })
+}
+
+export default textRollTriple
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import activateDropdown from './features/dropdown'
 import activateHeroSpin from './features/heroSpin'
+import textRollTriple from './features/textRoll'
 import createWallpaperAnim from './features/wallpaper'
 
 createWallpaperAnim()
 activateDropdown()
 activateHeroSpin()
+textRollTriple('.roll-text', '.roll-container')
